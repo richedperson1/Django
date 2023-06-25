d57a082516cb7911de706eb710a6b0e964a4021f
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-06-25 21:13:30 +0530

    adding setting about page in the django

--- firstDjango/helloworld/urls.py
+++ firstDjango/helloworld/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.helloWorld)
+    path("",views.helloWorld),
+    path("data",views.helloWorldData)
 ]
--- firstDjango/helloworld/views.py
+++ firstDjango/helloworld/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def helloWorld(request):
-    return HttpResponse("Hello world")
+    dist = {"Name":"Rutvik","lName":"Jaiswal"}
+    return render(request,"index.html",dist)
+
+
+def helloWorldData(request):
+    return render(request,"data.html")
