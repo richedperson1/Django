cb1aea176ede1572655bf1fab4eac94bcbe7ef2c
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-07-03 17:40:48 +0530

    adding aboutPage HTML file

--- firstDjango/aboutPage/urls.py
+++ firstDjango/aboutPage/urls.py
@@ -6,12 +6,12 @@ def dynamicURL(request,age):
     return HttpResponse(f"The age of student is {age}")
 
 def dynamicURL2(request,name):
-    return HttpResponse(f"The age of student is {name}")
+    return HttpResponse(f"The name of student is {name}")
 
 urlpatterns = [
     path("/<int:age>",dynamicURL),
-    path("/<str:name>",dynamicURL2),
-    path("/",views.aboutPage)
+    path("/",views.aboutPage),
+    path("/details",views.rendingDataAbout)
 ]
 
 
--- firstDjango/aboutPage/views.py
+++ firstDjango/aboutPage/views.py
@@ -5,3 +5,25 @@ from django.http import HttpResponse
 
 def aboutPage(request):
     return HttpResponse("This is the About page of the Django APP")
+
+
+def rendingDataAbout(request):
+    peopleData = [
+        {"name":"Rutvik",
+         "LastName":"Jaiswal",
+         "RollNum":26,
+         "CompanyName":"Amazon"
+         },
+        {"name":"Jadu",
+         "LastName":"Jaiswal",
+         "RollNum":26,
+         "CompanyName":"Harman"
+         },
+        {"name":"Prasad",
+         "LastName":"Jaiswal",
+         "RollNum":26,
+         "CompanyName":"Microsoft"
+         }
+    ]
+
+    return render(request,"aboutPage.html",context={"peopleData":peopleData})
