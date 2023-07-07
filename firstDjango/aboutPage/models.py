33a7e234c12581637b0b00fd4ceeda999ff903f4
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-07-07 16:18:26 +0530

    making model file for aboutPage app

--- firstDjango/aboutPage/models.py
+++ firstDjango/aboutPage/models.py
@@ -2,3 +2,12 @@ from django.db import models
 
 # Create your models here.
 # Python manage.py makemigrations
+
+# Create your models here.
+class StudentTable(models.Model):
+    firstName = models.CharField(max_length=23)
+    lastName  = models.CharField(max_length = 23)
+    mobNumber = models.IntegerField()
+    emailID   = models.EmailField()
+    # imageFile = models.ImageField()
+    age       = models.IntegerField()
