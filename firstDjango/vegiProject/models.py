a9d33cdbdf8b1760a08c048cc793d71f2e782d4e
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-07-29 23:18:05 +0530

    adding comments in the models

--- firstDjango/vegiProject/models.py
+++ firstDjango/vegiProject/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 # Create your models here.
+# The class "receipe" is a model in Python that represents a recipe with attributes such as dishName,
+# dishReceipe, and dishImg.
 
 class receipe(models.Model):
-
     dishName = models.CharField(max_length=100)
     dishReceipe = models.TextField()
     dishImg = models.ImageField(upload_to="dishImg")
