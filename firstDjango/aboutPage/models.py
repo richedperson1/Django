5b84c4321a70a5b624fc65f386646ade85ec0080
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-08-09 14:57:29 +0530

    Adding model to the django which is parent info

--- firstDjango/aboutPage/models.py
+++ firstDjango/aboutPage/models.py
@@ -17,3 +17,9 @@ class StudentTable(models.Model):
         return f"Name : {self.firstName}, Age : {self.age}"
     
     
+class parentInfo(models.Model):
+    studentName = models.CharField(max_length=50,verbose_name="name of student those parent info need to save")
+    fatherName = models.CharField(max_length=60,verbose_name="Student father name")
+    motherName = models.CharField(max_length=60,verbose_name="Student mother name")
+    mobileNumber = models.IntegerField(verbose_name="student guardian of number")
+    occupations = models.CharField(max_length=20)
