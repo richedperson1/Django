c5b9bf442d4f46f275dbcba58259058759690d3d
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-09-06 17:27:25 +0530

    adding receipe model in the server

--- firstDjango/vegiProject/admin.py
+++ firstDjango/vegiProject/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+# `admin.site.register("register")` is registering the model "register" with the Django admin site.
+# This allows the model to be managed and displayed in the admin interface.
+admin.site.register(receipe)
