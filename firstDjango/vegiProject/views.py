2728ce74420c1d3a768a29f4b622142d19ccd80d
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-09-06 17:23:07 +0530

    adding all working file uploading code. it will save dish name, dish receipe, dish image path in the server

--- firstDjango/vegiProject/views.py
+++ firstDjango/vegiProject/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 def renderVegi(request):
+    if request.method=="POST":
+        vegiUploadData = request.POST
+
+        dishNames   = vegiUploadData.get("dish-name")
+        dishReceipe = vegiUploadData.get("dish-receipe")
+        dishImage   = request.FILES.get("dish-image")
+
+        receipe.objects.create(
+            dishName = dishNames,
+            dishReceipe = dishReceipe ,
+            dishImg = dishImage
+        )
     return render(request,"vegiIndex.html")
